c90000657ead71e1a1f46569cd5e4ca7afbd5b1b
saraadelalhaddad <sara.alhaddad@colorado.edu>
2021-01-12 20:43:49 +0300

    signinhook

---
+++ components/SignInHook.js
@@ -0,0 +1,167 @@
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Controller, useForm } from "react-hook-form";
+
+import React from "react";
+import authStore from "../stores/authStore";
+
+export default function App() {
+  const { control, handleSubmit, errors, getValues } = useForm();
+  const usernameInputRef = React.useRef();
+  const passwordInputRef = React.useRef();
+  const phonenumberInputRef = React.useRef();
+
+  const onSubmit = async (user) => {
+    console.log(user);
+    await authStore.signin(user);
+    const loggedinUser = authStore.user;
+    console.log("from sign in>>", loggedinUser);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.label}>username</Text>
+        <Controller
+          name="username"
+          defaultValue=""
+          control={control}
+          rules={{ required: "Username is required." }}
+          onFocus={() => {
+            usernameInputRef.current.focus();
+          }}
+          render={({ onChange, onBlur, value }) => (
+            <TextInput
+              style={styles.input}
+              ref={usernameInputRef}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+        />
+      </View>
+
+      {errors.username && <Text>Incorrect Username. Sticky fingers?</Text>}
+
+      <View>
+        <Text style={styles.label}>Password</Text>
+        <Controller
+          name="password"
+          //   type={password}
+          defaultValue=""
+          control={control}
+          type="hidden"
+          rules={{
+            required: { value: true, message: "Password is required." },
+            pattern: {
+              value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+              message: "Incorrect",
+            },
+          }}
+          onFocus={() => {
+            passwordInputRef.current.focus();
+          }}
+          render={({ onChange, onBlur, value }) => (
+            <TextInput
+              style={styles.input}
+              ref={passwordInputRef}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+        />
+      </View>
+
+      {getValues("password") === "" && (
+        <Text>Oops you forgot your password.</Text>
+      )}
+
+      {errors.password && getValues("password") !== "" && (
+        <Text>Incorrect Password</Text>
+      )}
+
+      <View style={styles.button}>
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  label: {
+    color: "black",
+    margin: 20,
+    marginLeft: 0,
+  },
+  button: {
+    marginTop: 40,
+    color: "white",
+    backgroundColor: "#ec5990",
+    height: 40,
+    borderRadius: 4,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 8,
+    backgroundColor: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+    color: "black",
+  },
+});
+
+// const firstNameInputRef = React.useRef();
+// const lastNameInputRef = React.useRef();
+// <View>
+//         <Text style={styles.label}>First Name</Text>
+//         <Controller
+//           name="firstName"
+//           defaultValue=""
+//           control={control}
+//           rules={{ required: "This is required." }}
+//           onFocus={() => {
+//             firstNameInputRef.current.focus();
+//           }}
+//           render={({ onChange, onBlur, value }) => (
+//             <TextInput
+//               style={styles.input}
+//               ref={firstNameInputRef}
+//               onBlur={onBlur}
+//               onChangeText={(value) => onChange(value)}
+//               value={value}
+//             />
+//           )}
+//         />
+//       </View>
+
+//       {errors.firstName && <Text>This is required.</Text>}
+
+//       <View>
+//         <Text style={styles.label}>Last Name</Text>
+//         <Controller
+//           name="lastName"
+//           defaultValue=""
+//           control={control}
+//           rules={{ required: "This is required." }}
+//           onFocus={() => {
+//             firstNameInputRef.current.focus();
+//           }}
+//           render={({ onChange, onBlur, value }) => (
+//             <TextInput
+//               style={styles.input}
+//               ref={lastNameInputRef}
+//               onBlur={onBlur}
+//               onChangeText={(value) => onChange(value)}
+//               value={value}
+//             />
+//           )}
+//         />
+//       </View>
+
+//       {errors.lastName && <Text>This is required.</Text>}
--- components/navigator/Router.js
+++ components/navigator/Router.js
@@ -1,26 +1,29 @@
-import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Signin from "../Signin";
-import Signup from "../Signup";
+import AddItem from "../item/AddItem";
+import Categories from "../categories";
+import CategoryItemList from "../item/CategoryItemList";
+import DeleteButton from "../buttons/DeleteButton";
 import Home from "../Home";
-import NewItemList from "../item/NewItemList";
-import { observer } from "mobx-react";
-import MyProfile from "../profile/MyProfile";
-import UpdateProfile from "../profile/UpdateProfile";
-import ProfilePage from "../profile/ProfilePage";
-import ProfileList from "../profile/ProfileList";
 import ItemDetail from "../item/ItemDetail";
 import ItemList from "../item/ItemList";
-import Categories from "../categories";
-import authStore from "../../stores/authStore";
 import LogOutButton from "../buttons/LogOutButton";
-import CategoryItemList from "../item/CategoryItemList";
-import AddItem from "../item/AddItem";
-import DeleteButton from "../buttons/DeleteButton";
+import MyProfile from "../profile/MyProfile";
+import { NavigationContainer } from "@react-navigation/native";
+import NewItemList from "../item/NewItemList";
+import ProfileList from "../profile/ProfileList";
+import ProfilePage from "../profile/ProfilePage";
+import React from "react";
+import SignInHook from "../SignInHook";
+import SignUpHook from "../SignUpHook";
+import Signin from "../Signin";
+import Signup from "../Signup";
 import UpdateButton from "../buttons/UpdateButton";
 import UpdateItem from "../item/UpdateItem";
+import UpdateProfile from "../profile/UpdateProfile";
+import authStore from "../../stores/authStore";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { observer } from "mobx-react";
+
 // import { Tab } from "native-base";
 // const { Navigator, Screen } = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -57,6 +60,8 @@ const Router = () => {
       />
       <HomeStack.Screen name="Signup" component={Signup} />
       <HomeStack.Screen name="Signin" component={Signin} />
+      <HomeStack.Screen name="SignUpHook" component={SignUpHook} />
+      <HomeStack.Screen name="SignInHook" component={SignInHook} />
     </HomeStack.Navigator>
   );
 
@@ -70,7 +75,7 @@ const Router = () => {
         }}
       />
 
-      <ProfileStack.Screen name="ProfilePagge" component={ProfilePage} />
+      <ProfileStack.Screen name="ProfilePage" component={ProfilePage} />
       <ProfileStack.Screen
         name="UpdateProfile"
         component={UpdateProfile}
@@ -97,7 +102,7 @@ const Router = () => {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeStackScreen} />
         {authStore.user.id === 0 ? (
-          <Tab.Screen name="Signin" component={Signin} />
+          <Tab.Screen name="SignUpHook" component={SignUpHook} />
         ) : (
           <>
             <Tab.Screen name="MyProfile" component={ProfileStackScreen} />
@@ -105,7 +110,7 @@ const Router = () => {
           </>
         )}
 
-        <Tab.Screen name="Categoties" component={CategoryStackScreen} />
+        <Tab.Screen name="Categories" component={CategoryStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
